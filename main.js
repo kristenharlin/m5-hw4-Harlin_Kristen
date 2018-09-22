1a5bf5d7f12616081e43326a8265241d78876911
Kristen Harlin <kristenharlin@Kristens-MacBook-Pro.local>
2018-09-22 11:10:24 -0400

    add num string

---
+++ main.js
@@ -0,0 +1,17 @@
+// string
+
+var myJavaScriptPractice = "This is my JavaScript Practice";
+console.log(myJavaScriptPractice);
+
+
+//number
+
+var num = 6;
+console.log(num);
+
+var numAddition = 24 + 4;
+console.log(numAddition);
+
+var numConcatOne = 5;
+var numConcatTwo = "4";
+console.log(numConcatOne + numConcatTwo);
